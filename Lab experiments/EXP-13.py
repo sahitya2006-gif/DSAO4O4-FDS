31ea78fa83d90b139a05ebbce12e2e445ce3e1b6
sahitya2006-gif <192424217.simats@saveetha.com>
2026-08-03 11:00:38 +0530

    Add files via upload

---
+++ Lab experiments/EXP-13.py
@@ -0,0 +1,14 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/sanga/OneDrive/Attachments/Pictures/Documents/my work/fds exp 13.py
+Word Frequency Distribution:
+
+python : 3
+is : 3
+easy : 1
+to : 1
+learn : 1
+powerful : 1
+learning : 1
+fun : 1
---
+++ Lab experiments/EXP-14.py
@@ -0,0 +1,12 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/sanga/OneDrive/Attachments/Pictures/Documents/my work/fds exp 14.py
+Frequency Distribution of Customer Ages:
+
+Age
+22    3
+25    2
+28    1
+30    2
+Name: count, dtype: int64
