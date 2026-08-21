7490972d001d8c1b6b08349a1a659af76448b8c0
sahitya2006-gif <192424217.simats@saveetha.com>
2026-08-21 08:45:30 +0530

    Add files via upload

---
+++ Lab experiments/exp-21.py
@@ -0,0 +1,16 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+======================================= RESTART: C:/Users/sanga/OneDrive/Attachments/Pictures/Desktop/PYTHON PROGRAMMING LEAP TECH/question21.py =======================================
+Enter sample size: 10
+Enter confidence level (%): 95
+Enter desired precision: 1
+
+--- RESULTS ---
+Sample Size: 10
+Sample Mean: 12.85
+Standard Deviation: 0.7976
+Confidence Level: 95.0 %
+Confidence Interval: 12.3557 to 13.3443
+Margin of Error: 0.4943
+Desired precision is achieved.
